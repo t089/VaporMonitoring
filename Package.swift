9086109be463fe89f561ee77af563def4bc225cb
Tobias Haeberle <tobias.haeberle@holidu.com>
2018-11-14 12:30:07 +0100

    fix dependencies

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
-        .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.3.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
     ],
     targets: [
