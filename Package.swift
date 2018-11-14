b6313013e4accb02269b949f4541dff1369f92aa
Tobias Haeberle <tobias.haeberle@holidu.com>
2018-11-14 11:06:08 +0100

    fix missing dependency to `SwiftyJSON`

--- Package.swift
+++ Package.swift
@@ -10,10 +10,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
         .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.3.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
     ],
     targets: [
-      .target(name: "VaporMonitoring", dependencies: ["Vapor", "SwiftMetrics", "Leaf"]),
+      .target(name: "VaporMonitoring", dependencies: ["Vapor", "SwiftMetrics", "Leaf", "SwiftyJSON"]),
       .target(name: "MonitoringExample", dependencies: ["VaporMonitoring"])
     ]
 )
